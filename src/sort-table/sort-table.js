cbbeb48b6f635229940b0630efec617d7eec1b91
Audrey Carlsen <acarlsen@seattletimes.com>
2015-03-23 17:24:54 -0700

    adds csv parser

--- src/sort-table/sort-table.js
+++ src/sort-table/sort-table.js
@@ -8,7 +8,14 @@ require("./sort-table.less");
 
 var proto = Object.create(HTMLElement.prototype);
 
-proto.createdCallback = function() {};
+proto.createdCallback = function() {
+  var table = this.innerHTML.replace(/\r/g, "").split("\n").filter(function(line) { return line.match(/[^,]/) }).join("\n");
+  var parsed = [];
+  var parser = csv.parse({ columns: true });
+  parser.on("data", function(line) { parsed.push(line) });
+  parser.write(table);
+  parser.end();
+};
 proto.attachedCallback = function() {};
 proto.detachedCallback = function() {};
 proto.attributeChangedCallback = function() {};
